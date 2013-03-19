8a743f2ca12113f665efbd0eceb13afdbe36c413
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-19 22:40:58 +0200

    removed add_transition from automata.c

--- src/automata.c
+++ src/automata.c
@@ -1,46 +1,7 @@
 #include "token.h"
+#include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#define NEXT_HASH_MOD 7
-
-typedef struct node {
-    int index; // used only for debug
-    int *next[NEXT_HASH_MOD];
-    token_type_t type;
-
-    // Use this too keep track of array length
-    int sizes[NEXT_HASH_MOD];
-} node_t;
-
-node_t root;
-
-node_t **nodes;
-
-void add_transition(node_t *n, char c, int next) {
-    int next_index = (int)c % NEXT_HASH_MOD;
-    // No transition for this character yet
-    if (n->next[next_index] == NULL) {
-        n->next[next_index] = (int*)malloc(1 * sizeof(int));
-        if (n->next[next_index] == NULL) {
-            printf("Failed to allocate memory for transition: %d %c -> %d", n->index, c, next);
-        }
-        n->next[next_index][0] = next;
-        n->sizes[next_index] = 1;
-    } else {
-        // A transition has been added before
-        // Increase the size of @next array
-        int nodes_size = n->sizes[next_index] + 1;
-        int *temp = (int*)realloc(n->next[next_index], nodes_size * sizeof(int));
-        if (temp) {
-            n->next[next_index] = temp;
-            n->sizes[next_index] = nodes_size;
-        } else {
-            printf("Failed to reallocate memory for transition: %d %c -> %d", n->index, c, next);
-        }
-    }
-}
-
 
 void build_automata() {
     char line[128];
