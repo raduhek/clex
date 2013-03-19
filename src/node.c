556f00431cc4b7d18cde53d01798e3f36cfa9805
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-19 22:55:09 +0200

    added reaching_chars field on node structure

--- src/node.c
+++ src/node.c
@@ -1,16 +1,34 @@
 #include "node.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 node_t **nodes;
 
 void add_transition(node_t *n, char c, int next) {
+    int rc_limit;
     int next_index = (int)c % NEXT_HASH_MOD;
+
+    rc_limit = strlen(nodes[next]->reaching_chars);
+    // Do we have room for reaching characters?
+    if (rc_limit == REACH_CHAR_LIMIT ) {
+        printf(
+            "Node reaching_chars limit exceeded for node: %d with char %c from node %d (limit %d | chars %s)\n",
+            next,
+            c,
+            n->index,
+            rc_limit,
+            nodes[next]->reaching_chars
+        );
+        exit(10);
+    }
+
     // No transition for this character yet
     if (n->next[next_index] == NULL) {
         n->next[next_index] = (int*)malloc(1 * sizeof(int));
         if (n->next[next_index] == NULL) {
             printf("Failed to allocate memory for transition: %d %c -> %d", n->index, c, next);
+            exit(11);
         }
         n->next[next_index][0] = next;
         n->sizes[next_index] = 1;
@@ -24,6 +42,10 @@ void add_transition(node_t *n, char c, int next) {
             n->sizes[next_index] = nodes_size;
         } else {
             printf("Failed to reallocate memory for transition: %d %c -> %d", n->index, c, next);
+            exit(12);
         }
     }
+    
+    nodes[next]->reaching_chars[rc_limit] = c;
+    printf("%d\n", rc_limit);
 }
--- src/node.h
+++ src/node.h
@@ -4,11 +4,14 @@
 #include "token.h"
 
 #define NEXT_HASH_MOD 7
+#define REACH_CHAR_LIMIT 7
 
 typedef struct node {
     int index; // used only for debug
     int *next[NEXT_HASH_MOD];
     token_type_t type;
+
+    char reaching_chars[REACH_CHAR_LIMIT];
 
     // Use this too keep track of array length
     int sizes[NEXT_HASH_MOD];
