fea887b80cd0bd2c5c0017b1c65853044e0bebc2
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-20 00:50:07 +0200

    improved automata reading procedure

--- src/automata.c
+++ src/automata.c
@@ -5,9 +5,9 @@
 
 void build_automata() {
     char line[128];
-    // int k, j;
+    // int i, k, j;
     int number_of_states;
-    int i, state_index, next_state_index, token_type;
+    int state_index, next_state_index, token_type;
     char transition_char;
     FILE *spec_file = fopen("automata.data", "r");
 
@@ -18,47 +18,52 @@ void build_automata() {
 
     do {
         fgets(line, sizeof(line), spec_file);
-    } while ((line != NULL) && line[0] == '!');
+    } while ((line != NULL) && (line[0] == '\n' || line[0] == '!'));
 
     if (line == NULL) {
-        printf("automata.data is not properly formatted\n");
+        printf("automata.data is not properly formatted before reading number of states\n");
         exit (3);
     }
 
     // Get the number of states
     sscanf(line, "%d", &number_of_states);
-    // Initialize the nodes array
-    nodes =  malloc(number_of_states * sizeof (node_t*));
 
-    // Read other empty lines of comment lines
-    do {
-        fgets(line, sizeof(line), spec_file);
-    } while ((line != NULL) && line[0] == '!');
-    // Did we get to end of file already
-    if (line == NULL) {
-        printf("automata.data is not properly formatted\n");
-        exit (4);
+    // Initialize the nodes array
+    nodes = malloc(number_of_states * sizeof (node_t));
+
+    if (!nodes) {
+        printf("could not allocate memory for nodes\n");
+        exit(8);
     }
 
     // Next follow the states
-    for (i = 0; i <= number_of_states; ++i) {
-        node_t *n = (node_t*)malloc(sizeof(node_t));
-        sscanf(line, "%d : %d", &state_index, &token_type);
+    do {
         fgets(line, sizeof line, spec_file);
+        node_t *n;
+        if (line == NULL) {
+            printf("Could not read %d states from automata.data\n", number_of_states);
+            exit(5);
+        }
+        // Skip empty lines or comments
+        if (*line == '\n' || *line == '!') {
+            continue;
+        }
+        n = malloc(sizeof(node_t));
+        sscanf(line, "%d : %d", &state_index, &token_type);
 
         n->type = token_type;
         n->index = state_index;
         nodes[state_index] = n;
-    }
+        number_of_states--;
+    } while(number_of_states != 0); 
 
     // Here we have created our array of nodes
     // From here 'till EOF come transitions
-
     while (fgets(line, sizeof(line), spec_file) != NULL) {
         // Skip comments
-        if (line[0] == '!') continue;
+        if (*line == '\n' || *line == '!' ) continue;
         sscanf(line, "%d %c -> %d", &state_index, &transition_char, &next_state_index);
-
+        
         add_transition(nodes[state_index], transition_char, next_state_index); 
     } 
 
