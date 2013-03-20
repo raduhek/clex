72a412c34033e4d9696daea882bf045c99cae212
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-20 23:08:59 +0200

    added new token types and created token structure

--- src/token.c
+++ src/token.c
@@ -1,6 +1,13 @@
 #include "token.h"
 
-char *types_array[5] = {"BLANK", "IDENTIFIER", "KEYWORD", "OPERATOR", "COMMENT"};
+char *types_array[_NUMBER_OF_TOKENS] = {
+		"ROOT",
+		"BLANK",
+		"IDENTIFIER",
+		"KEYWORD",
+		"OPERATOR",
+		"COMMENT"
+	};
 
 char *get_token_type(token_type_t t) {
     return types_array[t];
--- src/token.h
+++ src/token.h
@@ -1,14 +1,30 @@
 #ifndef TOKEN_HG
 #define TOKEN_HG
 
-extern char *types_array[5];
+// Define first two state types for comparison
+#define ROOT_T 0
+#define BLANK_T 1
+
+// ROOT_I = index of root node
+#define ROOT_I 0
+
+#define _NUMBER_OF_TOKENS 7
+
+extern char *types_array[_NUMBER_OF_TOKENS];
 
 typedef enum {
+    ROOT,
+    BLANK,
     IDENTIFIER,
     KEYWORD,
     OPERATOR,
     COMMENT,
 } token_type_t;
+
+typedef struct token_struct {
+    token_type_t type;
+    char *value;
+} token_t;
 
 char *get_token_type(token_type_t t);
 #endif
