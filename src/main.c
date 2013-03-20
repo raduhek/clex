472d3921d89607fe82655f8503967101e8d1dc76
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-20 23:11:35 +0200

    primitive testing in main()

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "reader.h"
 #include "node.h"
 #include "automata.h"
+#include "utils.h"
+#include "lexer.h"
+#include "token.h"
 
 int main() {
+    token_t *t = (token_t*)malloc(sizeof(token_t));
     build_automata();
+    
+    prepare_lexer("test");
+
+    *t = get_token();
+
+    if(t) {
+        printf("cool\n");
+    } else {
+        printf("oops\n");
+    }
+
     return 0;
 }
