97394e13d24378ea0b85e1deaa14e8204bcf7ed7
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-30 00:51:01 +0200

    changed add_transition signature in node header file

--- src/node.h
+++ src/node.h
@@ -4,21 +4,27 @@
 #include "token.h"
 
 #define NEXT_HASH_MOD 7
-#define REACH_CHAR_LIMIT 7
+#define REACHERS_LIMIT 7
+
+typedef struct reach {
+    char c;
+    int node_index;
+} reach_t;
 
 typedef struct node {
     int index; // used only for debug
     int *next[NEXT_HASH_MOD];
     token_type_t type;
 
-    char reaching_chars[REACH_CHAR_LIMIT];
+    int reachers_count;
+    reach_t reachers[REACHERS_LIMIT];
 
     // Use this too keep track of array length
     int sizes[NEXT_HASH_MOD];
 } node_t;
 
 extern node_t **nodes;
-void add_transition(node_t *n, char c, int next);
+void add_transition(int state, char c, int next);
 node_t* get_next(node_t *current_node, char c); 
 
 #endif
