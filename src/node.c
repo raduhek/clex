7ed1e8048b4b3dcbe0374730212cd1077f9cf608
Cruceru Radu <radu.cruceru@gmail.com>
2013-03-30 00:49:14 +0200

    fixed major bug in adding transition/ hacked a fix for an alien bug

--- src/node.c
+++ src/node.c
@@ -5,10 +5,11 @@
 
 node_t **nodes;
 
-void add_transition(node_t *n, char c, int next) {
-    int rc_limit = 0;
+void add_transition(int state, char c, int next) {
+    int i, j;
     int next_index = (int)c % NEXT_HASH_MOD;
-
+    node_t *n = nodes[state];
+    int fuck_fix[NEXT_HASH_MOD];
 
     // No transition for this character yet
     if (n->next[next_index] == NULL) {
@@ -21,8 +22,7 @@ void add_transition(node_t *n, char c, int next) {
         n->sizes[next_index] = 1;
     } else {
         // Do we have room for reaching characters?
-        rc_limit = strlen(nodes[next]->reaching_chars);
-        if (rc_limit == REACH_CHAR_LIMIT ) {
+        if (nodes[next]->reachers_count == REACHERS_LIMIT) {
             exit(10);
         }
 
@@ -32,6 +32,7 @@ void add_transition(node_t *n, char c, int next) {
         int *temp = (int*)realloc(n->next[next_index], nodes_size * sizeof(int));
         if (temp) {
             n->next[next_index] = temp;
+            n->next[next_index][nodes_size - 1] = next;
             n->sizes[next_index] = nodes_size;
         } else {
             printf("Failed to reallocate memory for transition: %d %c -> %d", n->index, c, next);
@@ -39,7 +40,26 @@ void add_transition(node_t *n, char c, int next) {
         }
     }
     
-    nodes[next]->reaching_chars[rc_limit] = c;
+    for (i = 0; i < nodes[next]->reachers_count; ++i) {
+        if (nodes[next]->reachers[i].node_index == n->index && nodes[next]->reachers[i].c == c) {
+            return;
+        }
+    }
+
+    // this weird (fuck_fix) is here because
+    // when adding transition 19 P -> 4
+    // nodes[4]->sizes[1] changes from 1 to 19 (don't ask why
+    for (j = 0; j < NEXT_HASH_MOD; ++j) {
+        fuck_fix[j] = nodes[next]->sizes[j];
+    }
+
+    nodes[next]->reachers[i].node_index = n->index;
+    nodes[next]->reachers[i].c = c;;
+    nodes[next]->reachers_count++;
+
+    for (j = 0; j < NEXT_HASH_MOD; ++j) {
+        nodes[next]->sizes[j] = fuck_fix[j];
+    }
 }
 
 node_t* get_next(node_t *current_node, char c) {
@@ -51,14 +71,15 @@ node_t* get_next(node_t *current_node, char c) {
     // We know the bucket of nodes possible to be reached with char
     // Find the node that can actually be reached
     for (i = 0, k = current_node->sizes[next_index]; i < k; ++i) {
-        rc_size = strlen(nodes[current_node->next[next_index][i]]->reaching_chars);
+        rc_size = nodes[current_node->next[next_index][i]]->reachers_count;
 
         // WTF??
-        if (rc_size == 0) {
+        if (rc_size == 0 && current_node->next[next_index][i] != 0) {
             printf("Unreachable node: %d\n", current_node->next[next_index][i]);
         }
         for (j = 0; j < rc_size; ++j) {
-            if (nodes[current_node->next[next_index][i]]->reaching_chars[j] == c) {
+            if (nodes[current_node->next[next_index][i]]->reachers[j].node_index == current_node->index
+                && nodes[current_node->next[next_index][i]]->reachers[j].c == c) {
                 // Found it! Return it
                 return nodes[current_node->next[next_index][i]];
             }
